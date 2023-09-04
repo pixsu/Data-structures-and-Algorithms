e9c01856aba67d74bb7bb9e8039c2c893e49e105
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2023-09-04 21:08:05 +0800

    Create TotalEvenOdd.py

---
+++ Term 4/TotalEvenOdd.py
@@ -0,0 +1,18 @@
+#Exercise #2
+#Display total count of Even and Odd numbers
+
+data = [24, 3, 60, 7, 9, 11]
+
+even = []
+odd = []
+
+for num in data:
+    if num % 2 == 0: # checks if even number (24 % 2 == 0), true = even
+        even += 1
+    else:
+        odd += 1
+
+print("Total even number: "+ str(even))
+print("Total odd numbers: "+ str(odd))
+
+
