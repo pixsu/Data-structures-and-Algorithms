ba4e185e0bffec35558af0c5e4f8436c3f202ebc
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2023-09-04 21:07:20 +0800

    Create AscendingDescending.py

---
+++ Term 4/AscendingDescending.py
@@ -0,0 +1,12 @@
+#Exercise #1
+#Sort by ascending and descending order
+
+data = [24, 3, 60, 7, 9, 11]
+
+data.sort() #ascending order
+print("Ascending order: "+ str(data))
+
+data.sort(reverse=True) #descending order
+print("Descending order: "+ str(data))
+
+
